94f5d0be6a5ef501fde9b06e1740f4781295f8c1
Brahimovith <@gmail.com>
2023-11-23 06:04:19 -0800

    up

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 /**
  * main - entry point
@@ -10,6 +11,9 @@
 
 int main(int argc, char *argv[])
 {
+    clock_t debut,fin;
+    double tt;
+    debut = clock();
     FILE *fichier;
     char line[100];
     int i = 2;
@@ -23,7 +27,7 @@ int main(int argc, char *argv[])
     {
         return 1;
     }
-    while(fgets(line, sizeof(line), fichier))
+    while(fgets(line, sizeof(line), fichier) &&( (double)(fin - debut)/CLOCKS_PER_SEC)<=5 )
     {
         l = atoi(line);
         while(l % i)
@@ -32,6 +36,7 @@ int main(int argc, char *argv[])
         }
         a = l / i;
         printf("%d=%d*%d\n",l,a,i);
+	fin = clock();
     }
 
 
